742c5314643ee8369b6c7dc96f29a439065f02bb
NivruttiWagh <nivruttiwagh2002@gmail.com>
2025-06-12 15:04:46 +0530

    Spring Boot Security

---
+++ src/main/java/com/bridgelabz/D01_Spring_boot_Security/D01SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.D01_Spring_boot_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D01SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D01SpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/D01_Spring_boot_Security/D01SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.D01_Spring_boot_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D01SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
